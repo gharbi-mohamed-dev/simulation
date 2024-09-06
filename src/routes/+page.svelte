d56b3a808385d6bd00726b088603159dc0c8ba4a
Gharbi Mohamed <gharbi.mohamed.labaik@gmail.com>
2024-09-06 22:37:10 +0100

    add support for arabic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,103 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import Button from "$lib/components/ui/button/button.svelte";
-    import { Input } from "$lib/components/ui/input/index.js";
-    import { Label } from "$lib/components/ui/label/index.js";
-    import { SimulationSchema } from "$lib/validation";
-    import { Schema } from "@effect/schema";
-    import { Either } from "effect";
-    import { onMount } from "svelte";
-    import { writable } from "svelte/store";
+import { page } from "$app/stores";
+import Button from "$lib/components/ui/button/button.svelte";
+import { Input } from "$lib/components/ui/input/index.js";
+import { Label } from "$lib/components/ui/label/index.js";
+import { SimulationSchema } from "$lib/validation";
+import * as RadioGroup from "$lib/components/ui/radio-group/index.js";
+import { Schema } from "@effect/schema";
+import { Either } from "effect";
+import { onMount } from "svelte";
+import { writable } from "svelte/store";
+import { goto } from "$app/navigation";
+import { browser } from "$app/environment";
 
-    const getEntry = (name: string) => {
-        const entry = $page.url.searchParams.get(name);
+const getEntry = (name: string) => {
+	const entry = $page.url.searchParams.get(name);
 
-        if (entry) {
-            return Number(entry);
-        }
-    };
-    const data = writable<{
-        salary?: number;
-        months?: number;
-        previous?: number;
-        total?: number;
-        name?: string;
-        phone?: string;
-    }>({});
-    onMount(() => {
-        $data = {
-            salary: getEntry("salary"),
-            months: getEntry("months") || 24,
-            previous: getEntry("previous") || 0,
-            total: getEntry("total"),
-            name: $page.url.searchParams.get("name") || undefined,
-            phone: $page.url.searchParams.get("phone") || undefined,
-        };
-    });
-    $: either = Schema.decodeUnknownEither(SimulationSchema)($data);
-    $: formIsValid = Either.isRight(either);
-    $: console.log(either);
+	if (entry) {
+		return Number(entry);
+	}
+};
+const data = writable<{
+	salary?: number;
+	months?: number;
+	previous?: number;
+	total?: number;
+	name?: string;
+	phone?: string;
+}>({});
+const lang = writable<"ar" | "fr">(
+	$page.url.searchParams.get("lang") === "fr" ? "fr" : "ar",
+);
+
+onMount(() => {
+	$data = {
+		salary: getEntry("salary"),
+		months: getEntry("months") || 24,
+		previous: getEntry("previous") || 0,
+		total: getEntry("total"),
+		name: $page.url.searchParams.get("name") || undefined,
+		phone: $page.url.searchParams.get("phone") || undefined,
+	};
+});
+
+// const submit = async (e: SubmitEvent) => {
+//     const either = Schema.decodeUnknownEither(SimulationSchema)($data)
+
+//     if(Either.isLeft(either)) {
+
+//     }
+// }
+const changeLang = async (lang: string) => {
+	console.log({ lang });
+	if (browser && $page.url.searchParams.get("lang") !== lang) {
+		$page.url.searchParams.set("lang", lang);
+		await goto($page.url);
+	}
+};
+
+$: changeLang($lang);
+$: either = Schema.decodeUnknownEither(SimulationSchema)($data);
+$: formIsValid = Either.isRight(either);
+$: console.log(either);
 </script>
+<div
+class="w-full flex flex-col items-center justify-center"
+>
 
 <form
     action="/result"
     method="get"
-    class="w-full flex flex-col items-center justify-center p-4 gap-6"
+    class=" flex flex-col  p-4 gap-6"
 >
+<div class="flex items-center justify-between">
     <img class="h-16" src="/logo.png" alt="logo labaik tourisme et voyages" />
+        <input type="hidden" name="lang" value={$lang}>
+       <RadioGroup.Root  bind:value={$lang} >
+        <div class="flex items-center space-x-2">
+         <RadioGroup.Item value="fr" id="fr" />
+         <Label for="fr">Français</Label>
+        </div>
+        <div class="flex items-center space-x-2">
+         <RadioGroup.Item value="ar" id="ar" />
+         <Label for="ar">العربية</Label>
+        </div>
+       </RadioGroup.Root>
+</div>
+<div class="flex flex-col gap-6"
+dir="{$lang === 'ar' ? 'rtl': 'ltr'}"
+
+>
+
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="name">Nom</Label>
+        <Label class="capitalize" for="name">
+            {#if  $lang === 'fr'}
+                Nom
+            {:else}
+            اسم 
+            {/if}
+        </Label>
         <Input
             required
             name="name"
@@ -54,10 +105,20 @@
             id="name"
             bind:value={$data.name}
         />
-        <p class="text-muted-foreground text-sm">Votre Nom complet nom de famille + prenom</p>
+        <p class="text-muted-foreground text-sm">
+            {#if  $lang === 'fr'}
+            Votre Nom complet nom de famille + prenom
+            {:else}اسمك الكامل اسم العائلة + الاسم الأول
+            {/if}
+        </p>
     </div>
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="phone">Tel</Label>
+        <Label class="capitalize" for="phone">    
+            {#if  $lang === 'fr'}
+            Tel
+            {:else}هاتف
+            {/if}
+        </Label>
         <Input
             required
             name="phone"
@@ -65,10 +126,20 @@
             id="phone"
             bind:value={$data.phone}
         />
-        <p class="text-muted-foreground text-sm">Votre Numero de telephone</p>
+        <p class="text-muted-foreground text-sm">
+            {#if  $lang === 'fr'}
+            Votre Numero de telephone
+            {:else}رقم هاتفك{/if}
+
+        </p>
     </div>
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="salary">Salaire mensuel net</Label>
+        <Label class="capitalize" for="salary">
+            {#if  $lang === 'fr'}
+            Salaire mensuel net
+            {:else}الراتب الشهري الصافي
+            {/if}
+        </Label>
         <Input
             required
             name="salary"
@@ -76,12 +147,21 @@
             id="salary"
             bind:value={$data.salary}
         />
-        <p class="text-muted-foreground text-sm">Plus de 30000.00 da</p>
+        <p class="text-muted-foreground text-sm">
+            {#if  $lang === 'fr'}
+            Plus de 30000.00 da
+            {:else}الحد الأدنى 30000.00 دج
+            {/if}
+        </p>
     </div>
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="previous"
-            >capacité d'endettement consommée</Label
-        >
+        <Label class="capitalize" for="previous">
+            {#if  $lang === 'fr'}
+            capacité d'endettement consommée
+            {:else}
+            قيمة الديون الحالية شهريا للقروض السابقة
+            {/if}
+        </Label>
         <Input
             required
             name="previous"
@@ -91,11 +171,22 @@
             bind:value={$data.previous}
         />
         <p class="text-muted-foreground text-sm">
+            {#if  $lang === 'fr'}
             Mensualité des crédits en cours
+            {:else}
+            السداد الشهري للقروض المستحقة في الوقت الحالي
+           {/if}
         </p>
     </div>
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="n">Durée du financement</Label>
+        <Label class="capitalize" for="n">
+            {#if  $lang === 'fr'}
+            Durée du financement
+            {:else}
+            مدة التقسيط 
+
+            {/if}
+        </Label>
         <Input
             required
             name="months"
@@ -107,12 +198,22 @@
             step="1"
             bind:value={$data.months}
         />
-        <p class="text-muted-foreground text-sm">Choisir entre 12 et 24 mois</p>
+        <p class="text-muted-foreground text-sm">
+            {#if  $lang === 'fr'}
+            Choisir entre 12 et 24 mois
+            {:else}
+                الاختيار بين 12 و24شهرا
+            {/if}
+        </p>
     </div>
     <div class="flex w-full max-w-sm flex-col gap-1.5">
-        <Label class="capitalize" for="total"
-            >Le montant Totale : (OMRA / SEJOURS)</Label
-        >
+        <Label class="capitalize" for="total">
+            {#if  $lang === 'fr'}
+            Le montant Totale : (OMRA / SEJOURS)
+            {:else}
+            المبلغ الإجمالي ل: العمرة / الرحلة
+            {/if}
+        </Label>
         <Input
             required
             name="total"
@@ -124,10 +225,23 @@
         <p class="text-muted-foreground text-sm"></p>
     </div>
     {#if formIsValid}
-        <Button type="submit">Resultat de la simulation</Button>
+        <Button type="submit">
+            {#if  $lang === 'fr'}
+            Resultat de la simulation
+            {:else}
+            نتيجة المحاكاة
+            {/if}
+        </Button>
     {:else}
-        <Button type="button" variant="secondary"
-            >Resultat de la simulation</Button
-        >
+        <Button type="button" variant="secondary">
+            {#if  $lang === 'fr'}
+            Resultat de la simulation
+            {:else}
+            نتيجة المحاكاة
+            {/if}
+        </Button>
     {/if}
+</div>
+
 </form>
+</div>
--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -1,32 +1,50 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import * as Table from "$lib/components/ui/table";
-    import Button from "$lib/components/ui/button/button.svelte";
-    import type { PageServerData } from "./$types";
-    export let data: PageServerData;
-    const link = $page.url.search.toString();
+import { page } from "$app/stores";
+import * as Table from "$lib/components/ui/table";
+import Button from "$lib/components/ui/button/button.svelte";
+import type { PageServerData } from "./$types";
+export let data: PageServerData;
+const lang: "fr" | "ar" =
+	$page.url.searchParams.get("lang") === "fr" ? "fr" : "ar";
+$page.url.searchParams.set("lang", lang);
+
+const link = $page.url.search.toString();
 </script>
 
 <!-- 
 <pre>
     {result && JSON.stringify(result, null, 2)}
 </pre> -->
-<main class="w-full flex flex-col items-center justify-center p-4">
-    <Button variant="link" href="/{link.length > 0 ? `${link}` : ''}"
-        >Retour
+<main class="w-full flex flex-col items-center justify-center p-4"
+dir="{lang === 'ar' ? 'rtl': 'ltr'}"
+>
+    <Button variant="link" href="/{link.length > 0 ? `${link}` : ''}" class='underline'>
+        {#if lang === 'fr'}
+        Faire une nouvelle simulation
+        {:else}
+        تشغيل محاكاة جديدة
+        {/if}
     </Button>
     <Table.Root>
-        <Table.Caption>Résultats de la simulation</Table.Caption>
-        <Table.Header>
-            <Table.Row>
-                <Table.Head class="">Etapes</Table.Head>
-                <Table.Head>Valeur</Table.Head>
-            </Table.Row>
-        </Table.Header>
+        <Table.Caption>
+            {#if lang === 'fr'}
+            Résultats de la simulation
+            {:else}
+            نتائج المحاكاة
+            {/if}
+
+        </Table.Caption>
+      
         <Table.Body>
             <Table.Row>
                 <Table.Cell class="font-medium"
-                    >Remboursement mensuel</Table.Cell
+                    >
+                    {#if lang === 'fr'}
+                    Remboursement mensuel
+                    {:else}
+                    السداد الشهري
+                    {/if}
+                    </Table.Cell
                 >
                 <Table.Cell
                     >{data.success
@@ -35,21 +53,48 @@
                 >
             </Table.Row>
             <Table.Row>
-                <Table.Cell class="font-medium">Nombre de mois</Table.Cell>
+                <Table.Cell class="font-medium">
+                    {#if lang === 'fr'}
+                    Nombre de versement
+                    {:else}
+                    عدد الأقساط
+                    {/if}
+                </Table.Cell>
                 <Table.Cell
                     >{data.success ? data.result.aksat : "invalid"}</Table.Cell
                 >
             </Table.Row>
             <Table.Row>
-                <Table.Cell class="font-medium"
-                    >Cout De Revient De La Omra</Table.Cell
-                >
+                <Table.Cell class="font-medium">
+                    {#if lang === 'fr'}
+                    Montant du financement
+                    {:else}
+                    قيمة التمويل
+                    {/if}
+                </Table.Cell>
+                <Table.Cell
+                    >{data.success ? data.result.montantFinacement : "invalid"}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+                <Table.Cell class="font-medium">
+                    {#if lang === 'fr'}
+                    Cout De Revient De La Omra
+                    {:else}
+                    التكلفة الإجمالية بالتقسيط 
+                    {/if}
+                </Table.Cell>
                 <Table.Cell
                     >{data.success ? data.result.cost : "invalid"}</Table.Cell
                 >
             </Table.Row>
             <Table.Row>
-                <Table.Cell class="font-medium">Apport Initiale</Table.Cell>
+                <Table.Cell class="font-medium">
+                    {#if lang === 'fr'}
+                    Apport Initiale
+                    {:else}
+                    قيمة المساهمة الأولية
+                    {/if}
+                </Table.Cell>
                 <Table.Cell
                     >{data.success ? data.result.hamich : "invalid"}</Table.Cell
                 >
